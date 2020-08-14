1a59da971ea1a7d0b25bf53d013def2bc5e695fa
xulei.code <xulei.code@bytedance.com>
2020-08-14 18:25:26 +0800

    设置文本内容不显示

--- app/src/main/java/com/bytedance/gittest/MainActivity.kt
+++ app/src/main/java/com/bytedance/gittest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.bytedance.gittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         text.text = "你不好"
+
+        text.visibility = View.INVISIBLE
     }
 }
